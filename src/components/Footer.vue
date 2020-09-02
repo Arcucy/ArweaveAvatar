1d5306a7df580dcc3e2cf56a02bd21074b9cca5f
Ayaka Neko <neko@ayaka.moe>
2020-09-02 20:46:51 +0800

    :sparkles: Added Header and Footer
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

---
+++ src/components/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="notice">
+    Developed by
+    <a href="https://github.com/AyakaLab">Ayaka Lab</a>
+    for Project
+    <a href="https://github.com/AyakaLab/ArcLight">ArcLight</a>
+  </div>
+</template>
+
+<style scoped>
+.notice {
+  position: fixed;
+  color: rgb(54, 54, 54);
+  bottom: 30px;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  text-align: center;
+  animation-name: fadeInOpacity;
+  animation-iteration-count: 1;
+  animation-timing-function: ease;
+  animation-duration: 2s;
+}
+
+@media screen and (max-width: 400px){
+  .notice {
+    font-size: 12px;
+  }
+}
+
+a {
+  text-decoration: none;
+  color: #E56D9B;
+}
+
+@keyframes fadeInOpacity {
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <router-link to="./" class="header">
+      <img src="../assets/logo.png" class='logo'/>
+      <h2 class="title">Arweave Avatar</h2>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.header {
+  display: flex;
+  margin: 2rem auto;
+  justify-content: center;
+  align-items: center;
+}
+
+a {
+  text-decoration: none;
+  color: #000;
+}
+
+.logo {
+  margin-right: 1rem;
+  width: 100px;
+  height: 100px;
+
+  animation-name: jump;
+  animation-iteration-count: 1;
+  animation-timing-function: ease-in-out;
+  animation-duration: 2s;
+}
+
+@keyframes jump {
+  0%, 20%, 50%, 80%, 100% {transform: translateY(0);}
+  40% {transform: translateY(-30px);}
+  60% {transform: translateY(-15px);}
+}
+</style>
